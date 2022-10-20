9240be608b91019fa0ec39ad3fcaf1b232461e37
sezerking <sezerking123@gmail.com>
2022-10-20 11:37:45 +0300

    3. Derste kodda yaptığımız değişiklikler

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -3,11 +3,89 @@ var router = express.Router();
 
 // slash gördüğün zaman function çalışır
 const anaSayfa = function (req, res, next) {
-  res.render("anasayfa", { title: "Ana Sayfa" });
+  res.render("anasayfa", {
+    "baslik": "Anasayfa",
+    "sayfaBaslik": {
+      "siteAd": "Mekanbul",
+      "slogan": "Civardaki Mekanları Keşfet!",
+    },
+    "mekanlar":[
+      {
+        "ad":"Barida Kafe",
+        "adres":"Sdü Batı Kampüsü",
+        "puan":"4",
+        "mesafe":"2km",
+        "imkanlar":["Kahve","Çay","Pasta"]
+      },
+      {
+        "ad":"Gloria",
+        "adres":"Sdü Doğu Kampüsü",
+        "puan":"2",
+        "mesafe":"5km",
+        "imkanlar":["Kahve","Çay","Pasta"]
+      }
+
+
+
+    ]
+  });
 };
 
 const mekanBilgisi = function (req, res, next) {
-  res.render("mekanbilgisi", { title: "Mekan Bilgisi" });
+  res.render("mekanbilgisi",
+  { "baslik": "Mekan Bilgisi",
+   "mekanBaslik":"Barida Kafe",
+   "mekanDetay":{
+      "ad":"Barida Kafe",
+      "adres":"Sdü Batı Kampüsü",
+      "puan":"4",
+      "saatler":[
+        {
+          "gunler":"Pazartesi-Cuma",
+          "acilis":"9:00",
+          "kapanis":"23:00",
+          "kapali":"false"
+        },
+        {
+          "gunler":"Cumartesi-Pazar",
+          "acilis":"10:00",
+          "kapanis":"22:00",
+          "kapali":"false"
+        }
+      ],
+      "imkanlar":["Kahve","Çay","Pasta"],
+      "koordinatlar":{
+        "enlem":"37.7",
+        "boylam":"30.5"
+      },
+      "yorumlar":[
+        {
+          "yorumYapan":"Sinan",
+          "puan":"3",
+          "tarih":"20 Ekim 2022",
+          "yorumMetni":"Çok berbat"
+        },
+        {
+          "yorumYapan":"Sezer",
+          "puan":"2",
+          "tarih":"20 Ekim 2022",
+          "yorumMetni":"Düşmanıma içirmem"
+        },
+        {
+          "yorumYapan":"Karakaş",
+          "puan":"1",
+          "tarih":"20 Ekim 2022",
+          "yorumMetni":"Midesini seven içmez"
+        }
+      ]
+
+   }
+
+
+
+
+
+});
 };
 
 const yorumEkle = function (req, res, next) {
