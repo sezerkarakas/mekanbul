db595086f24ce3b918153f030ae0413e92198f4e
sezerking <sezerking123@gmail.com>
2022-11-20 20:35:33 +0300

    Deneme

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,5 +1,8 @@
 var mongoose=require("mongoose");
-var dbURI="mongodb+srv://mekan32:12345678a@mekanbul.5jgc9lr.mongodb.net/?retryWrites=true&w=majority";
+require("./mekansema");
+
+//var dbURI="mongodb://localhost/mekanbul";
+var dbURI="mongodb+srv://mekan32:12345678a@mekanbul.5jgc9lr.mongodb.net/mekanbul?retryWrites=true&w=majority";
 mongoose.connect(dbURI);
 
 function kapat(msg,callback){
@@ -17,7 +20,7 @@ process.on("SIGINT",function(){
 
 
 mongoose.connection.on("connected",function(){
-    console.log(dbURI+"adresindeki veritabanına bağandı.");
+    console.log(dbURI+" adresindeki veritabanına bağlandı.");
 })
 
 
@@ -26,9 +29,8 @@ mongoose.connection.on("disconnected",function(){
 })
 
 
-mongoose.connection.on("connected",function(){
+mongoose.connection.on("error",function(){
     console.log("Bağlantı hatası.");
 })
 
 
-require("./mekansema")
